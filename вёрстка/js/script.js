c1c8f4cd833346a56bb1fc08f829757ba016457c
AndrushcaPlay <49115741+AndrushcaPlay@users.noreply.github.com>
2019-03-31 11:38:43 +0300

    Загрузили 1 вермию
    Проверяем работу

---
+++ вёрстка/js/script.js
@@ -0,0 +1,26 @@
+var anim = anime({
+  targets: '#train',
+  translateY: -1050,
+  translateX: 850,
+  easing: 'easeInOutSine',
+  delay: 1000,
+  duration: 2000
+});
+
+ var anim2 = anime({
+  targets: '#train2',
+  translateY: -1050,
+  translateX: 850,
+  easing: 'easeInOutSine',
+  delay: 1000,
+  duration: 2000
+});
+
+  var anim1 = anime({
+  targets: '#train1',
+  translateY: -650,
+  translateX: 450,
+  easing: 'easeInOutSine',
+  delay: 2500,
+  duration: 2000
+});
